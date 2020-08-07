630baaf49ab38396dc3944eb6f53eee981eb3b70
Jonas Herseth <jonas.herseth@gmail.com>
2020-08-07 09:56:12 +0200

    Gruvterial v0.2 or something

---
+++ themes/gruvterial.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+THEME_VARIANT="light"
+THEME_LAPTOP_MODE="true"
+THEME_HEADER_BAR="light"
+THEME_PANEL="light"
+
+THEME_COLOUR_BACKGROUND="#F5EBD0"       # F5E7C4  # Background
+THEME_COLOUR_FOREGROUND="#F3E3BD"       # E8D8AE  # Accented background
+THEME_COLOUR_DIVIDER="#3d4220"          # 3d4220
+THEME_COLOUR_COMMENT="#83A598"          # 83A598 # Sliders, toggle button, checkboxes, radiobuttons, choice highlights
+THEME_COLOUR_DANGER="#fb4934"           # CC241D # Record button...
+THEME_COLOUR_MID_DANGER="#FE8019"
+THEME_COLOUR_WARNING="#FABD2F"          # FABD2F # scroll past end
+THEME_COLOUR_SUCCESS="#B8BB26"          # B8BB26 # "OK button" type buttons, Sliding switches, filled in area in sliders
+THEME_COLOUR_INFO="#979A1B"             # 8ec07c # Hover over some types of text buttons
+THEME_COLOUR_DARK_INFO="#FF0000"        # where? #0f3a42
+THEME_COLOUR_TEXT="#3c3836"             # regular text
+THEME_COLOUR_TEXT_HIGHLIGHT="#FF0000"   # where? #f0c6a6
+    
+THEME_COLOUR_ACCENT_PRIMARY="#979A1B"   # Link button # b8bb26
+THEME_COLOUR_ACCENT_SECONDARY="#979A1B" # b8bb26 # page1/2/3
+THEME_COLOUR_ACCENT_TERTIARY="#d3869b"  # Where? #bee4ea
+
+
+# Potential colors to use
+
+# #ebdbb2
+# #ebdbb2
+# #fb4934
+# #fe8019
+# #fabd2f
+# #b8bb26
+# #8ec07c
+# #83a598
+# #d3869b
+# #cc241d
+# #b8bb26
+# #fabd2f
+
+# #32302f
+# #3c3836
+# #3c3836
+# #504945
+# #504945
+# #665c54
+# #3c3836
+# #46413e
+# #5b534d
+# #3d4220
+# #424a3e
+# #472322
+# #543937
+# #0f3a42
+# #404946
+# #574833
+# #45403d
+
+# #f2e5bc
+# #eddeb5
+# #ebdbb2
+# #e6d5ae
+# #dac9a5
+# #d5c4a1
+# #ebdbb2
+# #ebdbb2
+# #dac9a5
+# #d1ea9b
+# #d7d9ae
+# #fbcab5
+# #f0c6a6
+# #bee4ea
+# #d3d5b8
+# #ecd6a3
+# #ebdbb2
